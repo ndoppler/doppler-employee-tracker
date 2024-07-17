5994412caa34421dc45a3c9eebc8f6908a5093cf
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-07-17 15:10:37 -0500

    addition of api routing for get department. next steps is to generate all api calls and then connect them to the CLI app

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const CLI = require('./lib/js/cli.js');
+const pool = require('./lib/db/config.js')
 
+pool.connect();
 const cli = new CLI();
 
 cli.run();
---
+++ lib/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO department (id, name)
+VALUES (01, 'Sales');
+
+SELECT * FROM department;
