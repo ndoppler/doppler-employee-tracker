041919e548e86450df0e133a255a1613f9b20217
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-07-19 20:57:35 -0500

    addition of add department cli option

--- lib/db/seeds.sql
+++ lib/db/seeds.sql
@@ -1,16 +1,16 @@
-INSERT INTO department (department_id, name)
-VALUES  (01, 'Sales'),
-        (02, 'Marketing'),
-        (03, 'Human Resources');
+INSERT INTO department (name)
+VALUES  ('Sales'),
+        ('Marketing'),
+        ('Human Resources');
 
 SELECT * FROM department;
 
-INSERT INTO role (role_id, title, salary, department_id)
-VALUES  (01, 'Salesperson', 50000, 01),
-        (02, 'Sales Manager', 75000, 01),
-        (03, 'Market Analyst', 60000, 02),
-        (04, 'Marketing Manager', 80000, 02),
-        (05, 'HR Representative', 35000, 03),
-        (06, 'HR Manager', 55000, 03);
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Salesperson', 50000, 01),
+        ('Sales Manager', 75000, 01),
+        ('Market Analyst', 60000, 02),
+        ('Marketing Manager', 80000, 02),
+        ('HR Representative', 35000, 03),
+        ('HR Manager', 55000, 03);
 
 SELECT * FROM  role;
